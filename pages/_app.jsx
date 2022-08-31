3be9547ed36ce568c858345354010257fb1b27bd
Nathaniel Hill <nathaniel@outdoorsy.co>
2022-08-30 21:39:04 -0500

    adding Trisagion

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,16 +9,13 @@ function App({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="Description" content="BuildSaintNicholas.com App" />
+        <meta name="Description" content="theosis.app" />
         <meta name="theme-color" content="#B8860B" />
-        <meta property="og:title" content="BuildSaintNicholas.com" />
-        <meta property="og:description" content="BuildSaintNicholas.com App" />
-        <meta
-          property="og:image"
-          content="https://buildsaintnicholas.com/og.png"
-        />
+        <meta property="og:title" content="theosis.app" />
+        <meta property="og:description" content="theosis.app" />
+        <meta property="og:image" content="https://theosis.app/og.png" />
         <link rel="manifest" href="/manifest.json" />
-        <title>BuildSaintNicholas.com</title>
+        <title>theosis.app</title>
       </Head>
       <div className="flex flex-col items-center justify-center w-screen min-h-screen text-gray-900 bg-brand-khaki">
         <header />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,61 @@
 import Cross from "@/components/Cross";
+import Trisagion from "@/components/Trisagion";
 
 const HomePage = () => (
   <>
-    <Cross />
+    <p>
+      <Trisagion />
+      {/* Troparia of Thanksgiving Now that the day has come to a close, I thank
+      thee, O Lord, and I ask that the evening with the night may be sinless;
+      grant this to me, O Saviour, and save me. Glory to the Father, and to the
+      Son, and to the Holy Spirit. Now that the day hath passed, I glorify thee,
+      O Master, and I ask that the evening, with the night may be without
+      offence; grant this to me, O Saviour, and save me. Both now and ever, and
+      unto ages of ages. Amen. Now that the day hath run its course, I praise
+      thee, O Holy One, and I ask that the evening with the night may be
+      undisturbed; grant this to me, O Saviour, and save me. Lord, have mercy.
+      (12 times) A Prayer for Forgiveness O Lord our God, if during this day I
+      have sinned, whether in word or deed or thought, forgive me all, for thou
+      art good and lovest mankind. Grant me peaceful and undisturbed sleep, and
+      deliver me from all influence and temptation of the evil one. Raise me up
+      again in proper time that I may glorify thee; for thou art blessed: with
+      thine Only-begotten Son and thine All-holy Spirit: now and ever, and unto
+      ages of ages. Amen. The Creed I believe in one God, the Father Almighty,
+      Maker of heaven and earth, and of all things visible and invisible; And in
+      one Lord Jesus Christ, the Son of God, the Only-begotten, Begotten of the
+      Father before all worlds, Light of Light, Very God of Very God, Begotten,
+      not made; of one essence with the Father, by whom all things were made:
+      Who for us men and for our salvation came down from heaven, And was
+      incarnate of the Holy Spirit and the Virgin Mary, and was made man; And
+      was crucified also for us under Pontius Pilate, and suffered and was
+      buried; And the third day He rose again, according to the Scriptures; And
+      ascended into heaven, and sitteth at the right hand of the Father; And He
+      shall come again with glory to judge the quick and the dead, Whose kingdom
+      shall have no end. And I believe in the Holy Spirit, the Lord, and Giver
+      of Life, Who proceedeth from the Father, Who with the Father and the Son
+      together is worshipped and glorified, Who spake by the Prophets; And I
+      believe in One Holy Catholic and Apostolic Church. I acknowledge one
+      Baptism for the remission of sins. I look for the Resurrection of the
+      dead. And the Life of the world to come. Amen. Prayer of the Hours O
+      Christ our God, who at all times and in every hour, in heaven and on
+      earth, art worshipped and glorified; who art long-suffering, merciful and
+      compassionate; who lovest the just and showest mercy upon the sinner; who
+      callest all to salvation through the promise of blessings to come; O Lord,
+      in this hour receive our supplications, and direct our lives according to
+      thy commandments. Sanctify our souls, hallow our bodies, correct our
+      thoughts, cleanse our minds; deliver us from all tribulation, evil and
+      distress. Encompass us with thy holy Angels, that guided and guarded by
+      them, we may attain to the unity of the faith and to the knowledge of
+      thine unapproachable glory, for thou art blessed unto ages of ages. Amen.
+      [Here may be added your own private devotions and intercessions, using
+      your own words or the "Occasional Prayers". When you have finished,
+      conclude with this prayer:] Through the prayers of our holy Fathers, Lord
+      Jesus Christ our God, have mercy upon us and save us. Amen. [As you lie
+      down to sleep, say:] Into thy hands, O Lord, I commend my soul and my
+      body. Do thou thyself bless me, have mercy upon me, and grant me life
+      eternal. Amen. */}
+    </p>
+    {/* <Cross /> */}
   </>
 );
 
